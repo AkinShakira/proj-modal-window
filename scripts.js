76f4daa770eb7e35fd47f6c3fa8d0f02a8058b76
AkinShakira <shakiraakinleye@gmail.com>
2022-06-23 12:56:37 +0100

    scripting

---
+++ scripts.js
@@ -0,0 +1,32 @@
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal-btn');
+const btnsShowModal = document.querySelectorAll('.show-modal-btn');
+
+const showModal = function () {
+  // My way
+  // modal.style.display = 'block';
+  // overlay.style.display = 'block';
+
+  // Tutorial
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsShowModal.length; i++)  {
+  btnsShowModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && (!modal.classList.contains('hidden'))) {
+      closeModal();
+    }
+});
